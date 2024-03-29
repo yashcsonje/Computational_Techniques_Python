c668207f08a66f379f9f4bef743f41fcc055596c
yashcsonje <162028752+yashcsonje@users.noreply.github.com>
2024-03-29 19:48:54 +0530

    Update IVT.py

--- IVT.py
+++ IVT.py
@@ -1,4 +1,9 @@
 #Intermediate value theorem
+'''
+If a function f(x) is continuous in th interval a,b and f(a) & f(b) are of opposite
+signs then equation f(x)=0 has atleast one root between x=a & x=b
+'''
+
 def function(x): 
     return 2*x**5 + 3*x**3 + 7*x - 13 
  
